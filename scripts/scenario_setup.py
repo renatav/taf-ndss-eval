b8871ce84383c948aaf74b2903510338f2754c57
Renata <rvaderna@openlawlib.org>
2025-10-28 03:25:06 +0100

    Reworking scenario setup

--- scripts/scenario_setup.py
+++ scripts/scenario_setup.py
@@ -1,53 +1,92 @@
 import os
+from pathlib import Path
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from scripts.shared import copy_dir, delete_dir, ensure_exists, rewire_remote
-from taf.git.repository import GitRepository
+from taf.git import GitRepository
+from taf.updater.updater import clone_repository, UpdateConfig
+from taf.updater.types.update import OperationType
 
-REPO_ROOT = "../repositories"
-
+REPO_ROOT = Path("../repositories")
+ORIGIN_DIR = REPO_ROOT / "origin"
+WORKSPACES_DIR = Path("../workspaces")
 
 
 def main():
     print("=== Starting scenario ===")
 
-    os.chdir(REPO_ROOT)
+    # initialize origin if it does not exist
+    for repo_name in ("cityofsanmateo/law", "cityofsanmateo/law-xml"):
+        repo_path = REPO_ROOT / repo_name
+        git_dir = repo_path / "git"
+        if git_dir.is_dir():
+            new_dir = git_dir.with_name(".git")
+            git_dir.rename(new_dir)
 
-    # Delete previous runs
-    delete_dir("origin")
-    delete_dir("user")
-    delete_dir("attacker")
+        origin_repo_path = ORIGIN_DIR / repo_name
+        origin_repo = GitRepository(path=origin_repo_path)
+        if not origin_repo.is_git_repository:
+            origin_repo.clone_from_disk(repo_path, is_bare=True)
 
-    # Ensure backups exist
-    ensure_exists("origin-bare", "origin-bare")
-    ensure_exists("origin-full", "origin-full")
+    current_scenario = "scenario1"
 
-    # Restore everything
-    copy_dir("origin-bare", "origin")
-    copy_dir("origin-full", "attacker")
-    copy_dir("origin-full", "user")
+    scenario_dir = WORKSPACES_DIR / current_scenario
+    if not scenario_dir.is_dir():
+        scenario_dir.mkdir(parents=True)
 
-    # Rewire remote for each namespace/repo pair
-    for namespace in os.listdir("origin"):
-        origin_ns_path = os.path.join("origin", namespace)
-        attacker_ns_path = os.path.join("attacker", namespace)
-        user_ns_path = os.path.join("user", namespace)
+    for actor in ("user", "attacker", "publisher"):
+        actor_dir = scenario_dir / actor
+        # clean if already exists
+        if actor_dir.is_dir():
+            delete_dir(actor_dir)
+            actor_dir.mkdir()
 
-        if not os.path.isdir(origin_ns_path):
-            continue
+        config = UpdateConfig(
+            operation=OperationType.CLONE,
+            remote_url=str((ORIGIN_DIR / "cityofsanmateo/law").resolve().absolute()),
+            path=str(actor_dir / "cityofsanmateo/law"),
+            update_from_filesystem=True,
+        )
 
-        for repo in os.listdir(origin_ns_path):
-            origin_repo = os.path.abspath(os.path.join(origin_ns_path, repo))
+        clone_repository(config)
 
-            attacker_repo = os.path.join(attacker_ns_path, repo)
-            user_repo = os.path.join(user_ns_path, repo)
+    # os.chdir(REPO_ROOT)
 
-            if os.path.isdir(attacker_repo):
-                rewire_remote(attacker_repo, origin_repo)
+    # # Delete previous runs
+    # delete_dir("origin")
+    # delete_dir("user")
+    # delete_dir("attacker")
 
-            if os.path.isdir(user_repo):
-                rewire_remote(user_repo, origin_repo)
+    # # Ensure backups exist
+    # ensure_exists("origin-bare", "origin-bare")
+    # ensure_exists("origin-full", "origin-full")
+
+    # # Restore everything
+    # copy_dir("origin-bare", "origin")
+    # copy_dir("origin-full", "attacker")
+    # copy_dir("origin-full", "user")
+
+    # # Rewire remote for each namespace/repo pair
+    # for namespace in os.listdir("origin"):
+    #     origin_ns_path = os.path.join("origin", namespace)
+    #     attacker_ns_path = os.path.join("attacker", namespace)
+    #     user_ns_path = os.path.join("user", namespace)
+
+    #     if not os.path.isdir(origin_ns_path):
+    #         continue
+
+    #     for repo in os.listdir(origin_ns_path):
+    #         origin_repo = os.path.abspath(os.path.join(origin_ns_path, repo))
+
+    #         attacker_repo = os.path.join(attacker_ns_path, repo)
+    #         user_repo = os.path.join(user_ns_path, repo)
+
+    #         if os.path.isdir(attacker_repo):
+    #             rewire_remote(attacker_repo, origin_repo)
+
+    #         if os.path.isdir(user_repo):
+    #             rewire_remote(user_repo, origin_repo)
 
     print("=== Scenario setup complete ===")
 
